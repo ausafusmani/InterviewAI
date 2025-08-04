72ff77713572f1456cfa08994668dda4c0a54b2f
ausafusmani <ausafshamsusmani@gmail.com>
2025-08-05 04:57:25 +0530

    page alignment fixed

--- frontend/interview-prep-ai/src/pages/InterviewPrep/components/RoleInfoHeader.jsx
+++ frontend/interview-prep-ai/src/pages/InterviewPrep/components/RoleInfoHeader.jsx
@@ -9,7 +9,7 @@ const RoleInfoHeader = ({
   lastUpdated,
 }) => {
   return (
-    <div className="bg-white relative">
+    <div className="bg-white relative ml-3">
       <div className="container mx-auto px-10 md:px-0">
         <div className="h-[200px] flex flex-col justify-center relative z-10">
           <div className="flex items-start">
