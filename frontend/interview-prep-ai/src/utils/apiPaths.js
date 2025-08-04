4c92731a0135eff85e21cf59b2f497a0f0134865
ausafusmani <ausafshamsusmani@gmail.com>
2025-08-05 04:44:37 +0530

    backend url added

--- frontend/interview-prep-ai/src/utils/apiPaths.js
+++ frontend/interview-prep-ai/src/utils/apiPaths.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://interviewai-6w9i.onrender.com";
+// export const BASE_URL = "http://localhost:8000";
 
 export const API_PATHS = {
   AUTH: {
